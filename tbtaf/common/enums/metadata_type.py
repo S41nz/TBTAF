5acc82b8d151ccb83a76011e76f34c45f95d5ebb
unknown <psainza@PSAINZA-LAP.oradev.oraclecorp.com>
2015-11-04 18:38:25 -0600

    Upload of foundation classes TBTAFResult and TBMetadata

---
+++ tbtaf/common/enums/metadata_type.py
@@ -0,0 +1,18 @@
+'''
+Created on 04/11/2015
+
+@author: S41nz
+'''
+
+class TBTAFMetadataType(object):
+    '''
+    Simple enumeration class that describes the types of metadata that can be discovered within a source code asset
+    '''
+    #Verdict enumeration types
+    
+    #The verdict for a passing test
+    TEST_CODE="Test Code"
+    #The verdict for a failed test
+    PRODUCT_CODE="Product Code"
+
+
---
+++ tbtaf/common/metadata.py
@@ -0,0 +1,52 @@
+'''
+Created on 04/11/2015
+
+@author: S41nz
+'''
+
+class TBMetadata(object):
+    '''
+    Class that encapsulates all the metadata decoration that can be discovered within a source code asset.
+    '''
+    
+    #Fields
+    #Empty list to store the discovered tags within the asset
+    tags = []
+    
+    #Methods
+
+
+    def __init__(self,metadataType):
+        '''
+        Constructor
+        '''
+        self.metadataType = metadataType
+    
+    def getMetadataType(self):
+        return self.metadataType
+    
+    def setAssetID(self,assetID):
+        self.assetID = assetID
+        
+    def getAssetID(self):
+        return self.assetID
+    
+    def setTags(self,tags):
+        self.tags = tags
+    
+    def getTags(self):
+        return self.tags
+    
+    def setPriority(self,priority):
+        self.priority = priority
+    
+    def getPriority(self):
+        return self.priority
+    
+    def setAssetDescription(self,desc):
+        self.description = desc
+        
+    def getAssetDescription(self):
+        return self.description
+    
+        
