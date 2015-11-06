197ff832fa48e872b02b90777f6f2d5540c524c5
eriflore <eriflore>
2015-11-06 11:28:36 -0600

    first check-in of orchestrator

---
+++ tbtaf/orchestrator/TBTAFOrchestrator.py
@@ -0,0 +1,50 @@
+'''
+Created on 02/11/2015
+
+@author: S41nz
+'''
+
+import time
+
+#class TBTAFOrchestrator():
+class TBTAFOrchestrator(object):
+    '''
+    Class that encapsulates the information from a given event within the TBTAF 
+    '''
+    
+    #Fields
+
+    def __init__(self, nameFile = "nameFileDefault.txt"):
+		#El parametro nameFile
+        '''
+        Constructor
+        '''
+		#leer archivo los parametros. Si no, tener unos parametros por default.
+		#buscar como tirar una initialiation exception
+		#buscar como tirar una invalid argument exception
+
+		#Initialize interpreter
+		#TBTAFInterpreter interpreter()
+		
+
+    def createNewProject(tBTestSuiteInstance, tBTestBedInstance, projectName):
+		#Invalid Argument Exception
+        
+    def parseScript(self, filePath):
+		#return interpreter.parseScript(filePath)
+
+    def createTestBed(self, urlList):
+        #if urlList is null
+			#urlList = "localhost"
+		
+		#Initialize TBTestBed
+		TBTestBed tBtestBedInstance
+		
+		return tBtestBedInstance
+    
+    def getEventSource(self):
+        return self.eventSource
+
+   
+        
+    
