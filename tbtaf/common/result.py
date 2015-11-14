80a86fef83b75055273041633b6248771623a019
miestrad <miestrad>
2015-11-14 16:09:17 -0600

    Initial implementation of TBTAFPublisher

--- tbtaf/common/result.py
+++ tbtaf/common/result.py
@@ -1,7 +1,9 @@
 '''
 Created on 03/11/2015
-
 @author: S41nz
+
+Modified on 14/11/2015 by @andresmuro
+Summary counters added to retrieve Suite results (passed,failed,inconclusive)
 '''
 
 class TBTAFResult(object):
@@ -16,13 +18,34 @@ class TBTAFResult(object):
         '''
         self.testVerdict = testVerdict
         self.resultSource = resultSource
+		self.passTests = 0
+        self.inconclusiveTests = 0
+        self.failedTests = 0
         
     def getVerdict(self):
         return self.testVerdict
     
     def getResultSource(self):
         return self.resultSource
-    
+		
+	def getPassTests(self):
+		return self.passTests
+
+	def getInconclusiveTests(self):
+		return self.inconclusiveTests
+		
+	def getFailedTests(self):
+		return self.failedTests		
+	
+	def setPassTests(self,passTests):
+		self.passTests = passTests
+
+	def setInconclusiveTests(self,inconclusiveTests):
+		self.inconclusiveTests = inconclusiveTests
+		
+	def setFailedTests(self,failedTests):
+		self.failedTests = failedTests
+	
     def setStartTimestamp(self,startTime):
         self.startTimestamp = startTime
     
---
+++ tbtaf/publisher/TBTAFPublisher.py
@@ -0,0 +1,67 @@
+'''
+Created on 07/11/2015
+
+@author: @andresmuro , @andres.alvarado , @mestradago , @rnunezc
+'''
+import datetime
+#class TBTAFPublisher():
+class TBTAFPublisher(object):
+    '''
+    The TBTAF Publisher is in charge of generating all the test 
+	execution related documentation.
+    '''
+	
+	def __init__(self):
+		#Constructor
+		
+	def PublishTestPlan(tBTestSuiteInstance, filePath, formatFlag):
+		'''
+		Builds a test plan specification on a desired format
+		based on the discovered metadata
+		'''
+		htmlFile = open(filePath,'w')
+		htmlFile.write(htmlString)
+		htmlFile.close()
+	
+	def PublishResultReport(tBTestSuiteInstance, filePath, formatFlag):
+		'''
+		Builds a test execution report on a desired format
+		based on the execution result of a given test suite
+		'''
+		#Read HTML Template file and put into a string
+		htmlTemplate = open('template.html','r')
+		htmlString = htmlTemplate.read()
+		#Get summary result from the test suite instance
+		summaryTestSuite = tBTestSuiteInstance.getSuiteResult()
+		#Calculate total number of test cases
+		totalTests = len(tBTestSuiteInstance.suiteTestCases)
+		#Calculate success rate of test summary results 
+		successRate = (float(summaryTestSuite.passTests) / float(totalTests))*100
+		s_successRate = format("%.2f" % successRate)
+		#Get start & end dates and format them for display 
+		startTime = summaryTestSuite.getStartTimestamp()
+		endTime = summaryTestSuite.getEndTimestamp()
+		s_startTime = startTime.strftime('%B %d,%Y %H:%M:%S')
+		s_endTime = endTime.strftime('%B %d,%Y %H:%M:%S')
+		#Calculate elapsed time and format it for display
+		elapsedTime = endTime - startTime
+		s_elapsedTime = str(elapsedTime)
+		#Calculate report time
+		reportTime = datetime.datetime.now()
+		s_reportTime = reportTime.strftime('%B %d,%Y %H:%M:%S')
+		
+				
+		#Replace the html string with summary results
+		htmlString = htmlString.replace('r_total_tests',total_tests)
+		htmlString = htmlString.replace('r_passed',summaryTestSuite.passTests )
+		htmlString = htmlString.replace('r_failed',summaryTestSuite.failedTests)
+		htmlString = htmlString.replace('r_inconclusive',summaryTestSuite.inconclusiveTests)
+		htmlString = htmlString.replace('r_success_rate',s_successRate + '%')
+		htmlString = htmlString.replace('r_start_time',s_start_time)
+		htmlString = htmlString.replace('r_end_time',s_end_time)
+		htmlString = htmlString.replace('r_end_time',s_elapsed_time)
+		htmlString = htmlString.replace('r_report_time',s_report_time)
+		htmlFile = open(filePath,'w')
+		htmlFile.write(htmlString)
+		htmlFile.close()
+		
