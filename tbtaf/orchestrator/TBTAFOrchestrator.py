3c0ac4e7263efc886f1f523634ed0d0e6e3038fd
Eric Flores <erflogo@gmail.com>
2015-11-22 23:09:40 -0600

    Methods init and createTestBed has been tested.

--- tbtaf/orchestrator/TBTAFOrchestrator.py
+++ tbtaf/orchestrator/TBTAFOrchestrator.py
@@ -5,179 +5,293 @@ Created on 02/11/2015
 '''
 
 import httplib
+import os
+import urlparse
+#import sys
+
 from common.test_bed import TBTestBed
 from common.project import TBProject
-from common.smart_suite import TBSmartTestSuite
 from common.node import TBTestNode
-from interpreter.interpreter import TBInterpreter
-from discoverer.discoverer import TBDiscoverer
+from common.suite import TBTestSuite
+from common.smart_suite import TBSmartTestSuite
 
-#from common.node import TBTestNode
+from common.enums.suite_type import TBTAFTestSuiteType
+from common.enums.filter_type import TBTAFFilterType
+
+#from interpreter.interpreter import TBInterpreter
+from discoverer.discoverer import TBTAFDiscoverer
+#from publisher.TBTAFPublisher import TBTAFPublisher
+from executor.Executor import TBTAFExecutor
 
 class TBTAFOrchestrator(object):
-    '''
-    
-    '''
-    
-    #Fields
-	#Declare global variables
-	#TBTAFInterpreter _interpreter
-	#projectList
-	#suiteTestCases = []
+	
+	INVALID_ARGUMENT_EXCEPTION_TEXT = 'Invalid Argument Exception'
+	
+	def __init__(self, nameInitizalizationFile = None):
+		if nameInitizalizationFile is None:
+			self.projectList = []
+		else:
+			if not self.isInvalidFile(nameInitizalizationFile):
+				self.projectList = []
+			else:
+				print 'Error: TBTAFOrchestrator.__init__'
 
-	def __init__(self, nameInitizalizationFile = ""):
-        '''
-        Constructor
-        '''
-		#leer archivo los parametros. Si no, tener unos parametros por default.
-		#buscar como tirar una initialiation exception
-		#buscar como tirar una invalid argument exception
-        if isInvalidArgument(nameInitizalizationFile):
-            print 'Error: TBTAFOrchestrator.__init__'
-        else:
-			projectList = []
-			#self._interpreter = TBTAFInterpreter()
-			#Initialize variables
-			#self.suiteType = suiteType
-			#self.suiteID = suiteID	
-
-	def addProject(self, newProject)
+	def addProject(newProject):
 		self.projectList.append(newProject)
-
-    def createNewProject(tBTestSuiteInstance, tBTestBedInstance, projectName):
+	
+	def createNewProject(self, tBTestSuiteInstance, tBTestBedInstance, projectName):
 		#Revisar si es la forma pythonesca y si funcionan.
-		if isInvalidArgument(tBTestSuiteInstance) or isInvalidArgument(tBTestBedInstance) or isInvalidArgument(projectName):
+		if self.isInvalidArgument(tBTestSuiteInstance) or self.isInvalidArgument(tBTestBedInstance) or self.isInvalidArgument(projectName):
 			print 'Error: TBTAFOrchestrator.createNewProject'
 		else:
-			if projectName in (projectInstance.projectName for projectInstance in projectList):
+			if projectName in [projectInstance.projectName for projectInstance in self.projectList]:
 				raise ValueError("Already Existing Project Exception")
 			else:
 				project =  TBProject(tBTestSuiteInstance, tBTestBedInstance, projectName)
 				self.addProject(project) 
 				#Agregar mensajes de confirmacion en linea de comandos
 				print 'New project created: ', projectName
-				#Duda: Si es un Smart Test Suite, hay que poner todos? porque no se reciben etiquetas como parámetro, sólo el TBTestSuiteInstance
-				#Duda: Qué pasa si llamamos a gestTestCases() para un smart suite... se corre el de TBTestSuite o de TBSmartTestSuite?
+				#Duda: Si es un Smart Test Suite, hay que poner todos? porque no se reciben etiquetas como parametro, solo el TBTestSuiteInstance
+				#Duda: Que pasa si llamamos a gestTestCases() para un smart suite... se corre el de TBTestSuite o de TBSmartTestSuite?
 				print 'Number of tests: ', len(tBTestSuiteInstance.getTestCases()) 
-				print 'Execution nodes: ', ', '.join(testNode.getNodeURL() for testNode in tBTestBedInstance.getTestBedNodes())
-        
-    def parseScript(self, filePath):
-		_interpreter = TBTAFInterpreter()
-		return _interpreter.parseScript(filePath)
-
-    def createTestBed(self, urlList = ['localhost']):
+				print 'Execution nodes: ', ', '.join([testNode.getNodeURL() for testNode in tBTestBedInstance.getTestBedNodes()])
+	'''
+	def parseScript(self, filePath):
+		return TBTAFInterpreter().parseScript(filePath)
+	'''
+    
+	def createTestBed(self, urlList = ['http://localhost']):
 		tBtestBedInstance = TBTestBed()
 		isValidUrlList = True
 		
-		if  isInvalidArgument(urlList):
+		if  self.isInvalidArgument(urlList):
 			print 'Error: TBTAFOrchestrator.createTestBed'
 		else:
 			for url in urlList:
-				if validateUrl(url):
+				if self.validateUrl(url):
 					tBtestBedInstance.addExecutionNode(url)
-				else
+				else:
 					isValidUrlList = False
-					print 'Invalid Argument exception'
 					print 'Error: TBTAFOrchestrator.createTestBed'
 					break
 				
 			if isValidUrlList:
 				print 'New test bed created on: ', urlList
 				return tBtestBedInstance
-
-	def isInvalidArgument(argument):
-		if argument == None or argument == "":
-			raise ValueError("Invalid Argument Exception")
-			return True
-		else:
-			return False
-			
-	def getServerStatusCode(url):
-		#https://pythonadventures.wordpress.com/2010/10/17/check-if-url-exists/
-		host, path = urlparse.urlparse(url)[1:3]
-		try:
-			conn = httplib.HTTPConnection(host)
-			conn.request('HEAD', path)
-			status = conn.getresponse().status
-			conn.close()
-			return status
-		except StandardError:
-			#Test this
-			conn.close()
-			return None
-	 
-	def validateUrl(url):
-		#https://pythonadventures.wordpress.com/2010/10/17/check-if-url-exists/
-		good_codes = [httplib.OK, httplib.FOUND, httplib.MOVED_PERMANENTLY]
-		valid = getServerStatusCode(url) in good_codes
-		return valid
+			else:
+				return
 	
 	#filePath - String containing the filepath where the test cases are located.
-	#smartFilePath - Optional string containing the filepath where the production source code is located. This would be useful for TBSmartTestSuite creation.
-	#tagList - String containing the list of tags which are desired to be executed among the existing test code within the specified location
-    def createTestSuite(self, filePath, smartFilePath, tagList):
-	
-	_discoverer = TBTAFInterpreter()
-	testCasesList = _discoverer.LoadTests(filePath)
-	
-	if
-	
-	for testCase in testCasesList
-		
-		
-	
-	#Primero hay que checar que las últimas dos variables no sean nulas
-	#Si no son deben contener ubicacion válidas
-	if smartFilePath is not null or tagList is not null
-		if validateExisitingLocations(smartFilePath) and validateExisitingLocations(tagList)
-			tbTestSuiteInstance = createTestSuite()
+	#tagList - Optional String containing the list of tags which are desired to be executed among the existing test code within the specified location.
+	def createTestSuite(self, filePath, tagList = None):	
+		if self.isInvalidPath(filePath):
+			print 'filePath'
+			print 'Error: TBTAFOrchestrator.createTestSuite'
+		else:
+			_discoverer = TBTAFDiscoverer()
+			testCaseList = _discoverer.LoadTests(filePath)
+			testSuiteID = 'testSuiteID_01'
 			
-	else
-		#Throw Invalid Argument Exception.
-	
+			if tagList is None:
+				_testSuite = TBTestSuite(TBTAFTestSuiteType.NORMAL, testSuiteID)
+				_testSuite.addTestCaseList(testCaseList)
+				
+				print 'Test ', testSuiteID, ' loaded from: ', filePath
+				print 'Test suite created with ', len(_testSuite.getTestCases()), ' test cases'
+				return _testSuite
+			else:
+				if self.isInvalidList(tagList):
+					print 'tagList'
+					print 'Error: TBTAFOrchestrator.createTestSuite'
+					return 
+				else:
+					_smartTestSuite = TBSmartTestSuite(testSuiteID)
+					_smartTestSuite.addTestCaseList(testCaseList)
+					
+					filteredTestCases = _testSuite.getTestCases(tagList, TBTAFFilterType.IN)
+					_smartTestSuite.removeTestCaseList()
+					_smartTestSuite.addTestCaseList(filteredTestCases)
+					
+					print 'Test ', testSuiteID, ' loaded from: ', filePath
+					print 'Smart test suite created with ', len(_smartTestSuite.getTestCases()), ' test cases'								
+					return _smartTestSuite	
 	
 	#tbTestSuiteInstance - Reference to a given TBTestSuite instance from which the test plan will be generated.
 	#testPlanLocation - String specifying the location where the test plan wants to be placed.
 	#outputFormat - Enumeration flag specifying the output format of the created test plan.
-	def publishTestPlan(self, tbTestSuiteInstance, testPlanLocation, outputFormat):
-		#TBTAFPublisher tbtafPublisherInstance
-		#tbtafPublisherInstance.PublishTestPlan(tbTestSuiteInstance, testPlanLocation, outputFormat)
-
+	'''
+	def publishTestPlan(self, tbTestSuiteInstance, testPlanLocation, outputFormat = 'html'):
+		#_publisher = TBTAFPublisher()
+		TBTAFPublisher().PublishTestPlan(tbTestSuiteInstance, testPlanLocation, outputFormat)
+	'''
 	#tbTestSuiteInstance - Reference to a given TBTestSuite instance from which the result report will be generated.
 	#resultLocation - String specifying the location where the result report wants to be placed.
 	#outputFormat - Enumeration flag specifying the output format of the created result report.
-	def publishTestResults(self, tbTestSuiteInstance, resultLocation, outputFormat):
-		#TBTAFPublisher tbtafPublisherInstance
-		#tbtafPublisherInstance.PublishTestResults(tbTestSuiteInstance, resultLocation, outputFormat)
-	
+	'''
+	def publishResultReport(self, tbTestSuiteInstance, resultLocation, outputFormat = 'html'):
+		TBTAFPublisher().PublishResultReport(tbTestSuiteInstance, resultLocation, outputFormat)
+	'''
 	#tbTestSuiteInstance - Reference to a given TBTestSuite instance from which the result report will be generated.
-    #tbTestSuiteInstance - Reference of the TBTestSuite representing the set of tests that will be executed.
-    #urlCollection - Optional collection of URLs corresponding to the nodes on which the test execution will be distributed.
-    #flagsCollection - Optional collection of flags that can customize the execution of such TBTestSuite.
-    #executorListenerCollection - Optional collection of ExecutorListener implementations that would want to listen and react to specific events during the execution of that specific TBTestSuite
+        #tbTestSuiteInstance - Reference of the TBTestSuite representing the set of tests that will be executed.
+        #urlCollection - Optional collection of URLs corresponding to the nodes on which the test execution will be distributed.
+        #flagsCollection - Optional collection of flags that can customize the execution of such TBTestSuite.
+        #executorListenerCollection - Optional collection of ExecutorListener implementations that would want to listen and react to specific events during the execution of that specific TBTestSuite
 	def excuteTestSuite(self, tbTestSuiteInstance, urlCollection, flagsCollection, executorListenerCollection):
-		#TBTAFExecutor tbtafExecutorInstance
-		return tbtafExecutorInstance.ExecuteTests(tbTestSuiteInstance, urlCollection, flagsCollection, executorListenerCollection)
+		#tbtafExecutorInstance = TBTAFExecutor()
+		return TBTAFExecutor().ExecuteTests(tbTestSuiteInstance, urlCollection, flagsCollection, executorListenerCollection)
 	
-    #projectName - String identifying the Project from where the query is being made.
-    #tagList - Optional List of tags in order to filter the query being made.
-    #indicatorEnumeration - If the list of tags is provided, then an additional enumeration can be passed in order to specify if the tags will be used to filter IN or OUT the results found.	
-	def GetTests(projectName, tagList, inOutList)
-				
+        #projectName - String identifying the Project from where the query is being made.
+        #tagList - Optional List of tags in order to filter the query being made.
+        #indicatorEnumeration - If the list of tags is provided, then an additional enumeration can be passed in order to specify if the tags will be used to filter IN or OUT the results found.	
+	def getTests(self, projectName, tagList = [], filterType = None):
 		#If the parameters being passed are null or contain null then an Invalid Argument Exception will be thrown.
 		#If the provided Project key does not exist on the currently connected datasource, then a Not Existing Project Exception will be thrown.
-		#If the provided filtering enumeration is provided, then a Not Supported Filter Exception will be thrown.
-
-		return _tBTestSuiteInstance.getTestCases(tagList, inOutList)
+		#If the provided filtering enumeration is provided, then a Not Supported Filter Exception will be thrown. -- What's this?
+		if self.isInvalidArgument(projectName):
+			print 'Error: TBTAFOrchestrator.GetTests'
+		elif not self.isExistingProject(projectName):
+			print 'Error: TBTAFOrchestrator.GetTests'
+		elif self.isInvalidList(tagList):
+			print 'Error: TBTAFOrchestrator.GetTests'
+		#I think this is what Pablo means in the iii. exception description. 
+		#Do I need to use the TBTAFFilterType.IN or TBTAFFilterType.OUT values?
+		elif not self.isSupportedFilter(filterType):
+			print 'Error: TBTAFOrchestrator.GetTests'
+		else:
+			#Get TBProject object related to our projectName
+			projectInstance = next((project for project in self.projectList if project.projectName == projectName), None)
+			#Get TBTestSuite object for our projectInstance
+			tbTestSuite = projectInstance.getTBTestSuite()
+			#vale la pena agregar excepcion si se provee lista de tags y el proyecto tiene un TBTestSuite y no smart?
+			#si el proyecto tiene ligado un TBTestSuite, como puedo usar getTestCases() de TBSmartTestSuite para filtrar por Tag. En caso que siguiente exception no deba levantarse.
+			if tbTestSuite.getTestSuiteType == TBTAFTestSuiteType.NORMAL and len(tagList):
+				raise ValueError("Normal TBTestSuite not compatible with Tag list Exception")
+				print 'Error: TBTAFOrchestrator.GetTests'
+			else:
+				#Check what type of Test Suite that project has and action accordingly
+				#Get initial list of test cases. This depends on what type of TBTestSuite we have. 
+				#Al parecer la lista de tags es para filtar que test cases quieres obtener del total del proyecto. 
+				if tbTestSuite.getTestSuiteType == TBTAFTestSuiteType.NORMAL or not len(tagList):
+					queriedTests = tbTestSuite.getTestCases()
+				else:
+					#Do I need to append list each time I get from getTestCases?
+					#Should we suggest getting a list in getTestCases() for inOutList (list of filterType)
+					if filterType is not None:
+						queriedTests = tbTestSuite.getTestCases(tagList, filterType)
+					else:
+						queriedTests = tbTestSuite.getTestCases(tagList) #Asi se pueden omitir parametros opcionales? Y si mejor le pongo el filterType siempre?
+				#Duda: En el diccionario, debo tener un tag por cada tag del proyecto, aunque este filtrado por el parametro de tagList? O, si es filtrado, lo saco del diccionario.
+				tagDictionary = self.getTags(projectName)
+				#Create empty dictionary
+				# Empty dict
+				d = {}
+				for tag in tagDictionary:
+					d[tag] = self.getTestsForTag(queriedTests, tag)
+				for tagkey, testList in d.items():
+					print tagkey, ': ', ', '.join(testList) #Crear mas bien lista de Test Files o Test IDs. Hay un atributo con test file o test id en TBTestCase?
+				#Should I return list of Test Files or list of Test Cases objects?
+				return d  
+				
+	def getTestsForTag(testList, tag): 
+		'''
+		Method to obtain the tests that contain the tag from the input
+		'''
+		dataSet = testList
+		resultTestCases = []
+		#Check if the base data set is not None
+		if dataSet is not None:
+			for candidateTest in dataSet:
+				#Fetch the test metadata
+				testMetadata = candidateTest.getTestMetadata()
+				if testMetadata is not None:
+					testTags = testMetadata.getTags()
+					if tag in testTags:
+						resultTestCases.append(candidateTest)
+		return resultTestCases
 
 	#projectName - String describing the project from which the query is being made.
-	def GetTags(projectName)
-		tagList[]
-		testCase in getProject(projectName).tbTestSuiteInstance.getTestCases()
-			tagList.append(testCase.getMetadata.getTags)
-		return tagList
-		#Excepciones
-		#If null string is provided as Project ID then an Invalid Argument Exception will be thrown.
+	def getTags(self, projectName):
+		if self.isInvalidArgument(projectName):
+			print 'Error: TBTAFOrchestrator.GetTags'
+		elif not self.isExistingProject(projectName):
+			print 'Error: TBTAFOrchestrator.GetTags'
+		else:
+			tagList = []
+			projectInstance = next((project for project in self.projectList if project.projectName == projectName), None)
+			tbTestSuiteInstance = projectInstance.getTBTestSuite()
+			testCases = tbTestSuiteInstance.getTestCases()
+			for testCase in testCases:
+				testMetadata = testCase.getTestMetadata()
+				if testMetadata is not None:
+					testTags = testMetadata.getTags()
+					for tag in testTags:
+						if tag not in tagList:
+							tagList.append(tag)
+			print 'Tags: ', ', '.join(tagList)
+			return tagList
+		
+	#Validations
 	
-    def getEventSource(self):
-        return self.eventSource
+	def isInvalidArgument(self, argument):
+		if argument == None or argument == "":
+			raise ValueError(self.INVALID_ARGUMENT_EXCEPTION_TEXT)
+			return True
+		return False
+			
+	def isInvalidList(self, list):
+		if not all(list):
+			raise ValueError(self.INVALID_ARGUMENT_EXCEPTION_TEXT)
+			return True
+		return False
+		
+	def isInvalidPath(self, path):
+		if not os.path.exists(path):
+			raise ValueError(self.INVALID_ARGUMENT_EXCEPTION_TEXT)
+			return True
+		return False
+		
+	def isInvalidFile(self, fileName):
+		if not os.path.isfile(fileName):
+			print 'Invalid file: ', fileName
+			raise ValueError(self.INVALID_ARGUMENT_EXCEPTION_TEXT)
+			return True
+		return False
+		
+	def isSupportedFilter(self, filterType):
+		if (filterType == TBTAFFilterType.IN or filterType == TBTAFFilterType.OUT or filterType is None):
+			return True
+		raise ValueError("Not Supported Filter Exception")
+		return False
+
+	def isExistingProject(self, projectName):
+		if projectName in [projectInstance.projectName for projectInstance in self.projectList]:
+			return True
+		raise ValueError("Not Existing Project Exception")
+		return False
+			
+	def getServerStatusCode(self, url):
+		#https://pythonadventures.wordpress.com/2010/10/17/check-if-url-exists/
+		host, path = urlparse.urlparse(url)[1:3]
+		#print 'host', host
+		#print 'path', path
+		try:
+			conn = httplib.HTTPConnection(host)
+			conn.request('HEAD', path)
+			status = conn.getresponse().status
+			#print 'status:', status
+			conn.close()
+			return status
+		except StandardError:
+			#conn.close()
+			return None
+	 
+	def validateUrl(self, url):
+		#https://pythonadventures.wordpress.com/2010/10/17/check-if-url-exists/
+		good_codes = [httplib.OK, httplib.FOUND, httplib.MOVED_PERMANENTLY]
+		status = self.getServerStatusCode(url)
+		#print 'status:', status
+		valid = status in good_codes		
+		if not valid:		
+			print 'Invalid URL:', url
+			raise ValueError(self.INVALID_ARGUMENT_EXCEPTION_TEXT)
+		return valid
