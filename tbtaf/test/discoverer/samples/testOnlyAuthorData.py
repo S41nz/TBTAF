f029c8ed296331e7338e6e93a48ea436e25b07e9
A01221624 <rgaribay@itesm.mx>
2015-11-29 10:02:01 -0600

    Adjust class names of sample tests and line breaks

--- tbtaf/test/discoverer/samples/testOnlyAuthorData.py
+++ tbtaf/test/discoverer/samples/testOnlyAuthorData.py
@@ -11,7 +11,7 @@ from common.enums.verdict_type import TBTAFVerdictType
 from common.enums.event_type import TBTAFEventType
 
 
-class DiscovererCompleteAndValidTest(TBTestCase):
+class TestOnlyAuthorDataNoMetadata(TBTestCase):
     '''
     classdocs
     '''
@@ -22,23 +22,24 @@ class DiscovererCompleteAndValidTest(TBTestCase):
         Constructor
         '''
         self.testTimeout = 1984
+        self.testName = self.__class__.__name__
 
     def setup(self):
         TBTestCase.setup(self)
-        print "Setup performed from DiscovererCompleteAndValidTest"
-        self.testTrace = TBTAFTrace("DiscovererCompleteAndValidTest")
-        self.testTrace.addEvent(TBTAFEvent(TBTAFEventType.INFO,"Setup performed from DiscovererCompleteAndValidTest",self.testTrace.getTraceSource()))
-        
-
+        print "Setup performed from " + self.testName
+        self.testTrace = TBTAFTrace(self.testName)
+        setup_str = "Setup performed from " + self.testName
+        self.testTrace.addEvent(TBTAFEvent(TBTAFEventType.INFO,
+                                           setup_str,
+                                           self.testTrace.getTraceSource()))
 
     def execute(self):
         TBTestCase.execute(self)
-        print "Execute performed from DiscovererCompleteAndValidTest"
-        self.testResult = TBTAFResult(TBTAFVerdictType.PASS,"DiscovererCompleteAndValidTest")
-
+        print "Execute performed from " + self.testName
+        self.testResult = TBTAFResult(TBTAFVerdictType.PASS,
+                                      self.testName)
 
     def cleanup(self):
         TBTestCase.cleanup(self)
-        print "Cleanup performed from DiscovererCompleteAndValidTest"
-
+        print "Cleanup performed from " + self.testName
         
--- tbtaf/test/discoverer/samples/testPriorityAboveLimit.py
+++ tbtaf/test/discoverer/samples/testPriorityAboveLimit.py
@@ -19,7 +19,7 @@ from common.enums.verdict_type import TBTAFVerdictType
 from common.enums.event_type import TBTAFEventType
 
 
-class DiscovererOnlyTheRequiredTest(TBTestCase):
+class TestPriorityAboveLimit(TBTestCase):
     '''
     classdocs
     '''
@@ -30,23 +30,25 @@ class DiscovererOnlyTheRequiredTest(TBTestCase):
         Constructor
         '''
         self.testTimeout = 1984
+        self.testName = self.__class__.__name__
 
     def setup(self):
         TBTestCase.setup(self)
-        print "Setup performed from DiscovererOnlyTheRequiredTest"
-        self.testTrace = TBTAFTrace("DiscovererOnlyTheRequiredTest")
-        self.testTrace.addEvent(TBTAFEvent(TBTAFEventType.INFO,"Setup performed from DiscovererOnlyTheRequiredTest",self.testTrace.getTraceSource()))
-        
-
+        print "Setup performed from " + self.testName
+        self.testTrace = TBTAFTrace(self.testName)
+        setup_str = "Setup performed from " + self.testName
+        self.testTrace.addEvent(TBTAFEvent(TBTAFEventType.INFO,
+                                           setup_str,
+                                           self.testTrace.getTraceSource()))
 
     def execute(self):
         TBTestCase.execute(self)
-        print "Execute performed from DiscovererOnlyTheRequiredTest"
-        self.testResult = TBTAFResult(TBTAFVerdictType.PASS,"DiscovererOnlyTheRequiredTest")
-
+        print "Execute performed from " + self.testName
+        self.testResult = TBTAFResult(TBTAFVerdictType.PASS,
+                                      self.testName)
 
     def cleanup(self):
         TBTestCase.cleanup(self)
-        print "Cleanup performed from DiscovererOnlyTheRequiredTest"
-
+        print "Cleanup performed from " + self.testName
+        
         
