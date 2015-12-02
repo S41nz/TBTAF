8a3f0a9f328c41b7ab826dedce21a335c50da931
unknown <psainza@PSAINZA-LAP.oradev.oraclecorp.com>
2015-12-02 00:18:50 -0600

    Test content for integration testing

---
+++ test/smoke/sample_test02.py
@@ -0,0 +1,53 @@
+'''
+<TBTAF>
+<TestID>1002</TestID>
+<Tags>TBTAF,Smoke</Tags>
+<Priority>2</Priority>
+<Description>Hello from Sample test 1002</Description>
+</TBTAF>
+'''
+from common.test import TBTestCase
+from common.result import TBTAFResult
+from common.trace import TBTAFTrace
+from common.event import TBTAFEvent
+from common.enums.verdict_type import TBTAFVerdictType
+from common.enums.event_type import TBTAFEventType
+from common.metadata import TBMetadata
+import time
+
+class TBTAFSampleTest(TBTestCase):
+    '''
+    classdocs
+    '''
+
+    
+        
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.testResult = TBTAFResult(TBTAFVerdictType.INCONCLUSIVE,"TBTAFSampleTest")
+        self.testTrace = None
+        self.testTimeout = 1984
+        self.testMetadata = TBMetadata('TEST')
+        
+    def setup(self):
+        TBTestCase.setup(self)
+        print "Setup performed from TBTAFSampleTest"
+        self.testTrace = TBTAFTrace("TBTAFSampleTest")
+        self.testTrace.addEvent(TBTAFEvent(TBTAFEventType.INFO,"Setup performed from TBTAFSampleTest",self.testTrace.getTraceSource()))
+        
+
+
+    def execute(self):
+        time.sleep(7)
+        TBTestCase.execute(self)
+        print "Execute performed from TBTAFSampleTest"
+        self.testResult = TBTAFResult(TBTAFVerdictType.PASS,"TBTAFSampleTest")
+
+
+    def cleanup(self):
+        TBTestCase.cleanup(self)
+        print "Cleanup performed from TBTAFSampleTest"
+
+        
