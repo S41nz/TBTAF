72f08fcc4a2fee54f51061c7be7fca5b5c9a1f8d
Robert Carrillo <robertcl795.rc@gmail.com>
2023-12-08 15:58:33 -0600

    fix: remove unused

--- client/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), { provide: MonacoEditorModule }],
-};
